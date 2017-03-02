ffe6bdf4417309b1a78378f360ffbba469f9a662
Edward Goo <Edwardx89@yahoo.com>
2017-03-02 14:09:12 -0500

    started working on it

---
+++ bubblesort.js
@@ -0,0 +1,15 @@
+function swap(index1, index2, array){
+var greater = arr[index1];
+array[index1] = array[index2];
+array[index2] = greater;
+};
+
+function bubbleSort(array){
+
+for(var i = 0; i < array.length; i++){
+  if(array[i] > array[i+1]){
+    swap(i,i+1, array);
+  }
+}
+return array;
+};
---
+++ bubblesort.spec.js
@@ -0,0 +1,27 @@
+describe('Bubble Sort', function(){
+  beforeEach(function () {
+    spyOn(window, 'swap').and.callThrough();
+  });
+
+  it('handles an empty array', function (){
+    expect( bubbleSort([]) ).toEqual( [] );
+    expect(window.swap.calls.count()).toEqual(0);
+  });
+  it('handles an array of 1', function (){
+    expect( bubbleSort( [1] ).toEqual ([1]))
+    expect(window.swap.calls.count()).toEqual(0);
+  });
+  it('handles an array of elements', function (){
+    expect( bubbleSort( [1,9,5,6,3]).toEqual ( [1,3,5,6,9]))
+    expect(window.swap.calls.count()).toEqual(5);
+  })
+  it('handles an array of elements in order', function (){
+    expect( bubbleSort( [1,2,3,4,5]).toEqual ( [1,2,3,4,5] ))
+    expect(window.swap.calls.count()).toEqual(0);
+  })
+  it('handles an array of elements', function (){
+    expect( bubbleSort( [9,6,5,3,1]).toEqual ( [1,3,5,6,9] ))
+    expect(window.swap.calls.count()).toEqual(10);
+  })
+});
+
