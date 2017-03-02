ba5fc9aa8297bbc614ba50cf52a1ee0122454afe
Edward Goo <Edwardx89@yahoo.com>
2017-03-02 17:39:43 -0500

    updated

--- mergesort.js
+++ mergesort.js
@@ -5,11 +5,16 @@ function mergeSort(array){
  var leftArray = array.slice(0,array.length/2)
  var rightArray = array.slice(array.length/2,array.length);
 
-  if(array.length === 1){
+  if(array.length === 0 || array.length ===1 ){
     return array;
-  }
+  } else {
+
   sortedLeftArray = mergeSort(leftArray);
   sortedRightArray = mergeSort(rightArray);
+  }
 
 
+return result;
 }
+
+console.log(mergeSort([9,-6,5,-3,1,2]));
